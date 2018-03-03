cbdb5db08cac002485c340a9907c9726ffd43dea
John Olubori David <daveholuborhee@gmail.com>
2018-03-03 21:47:59 +0100

    Refactor the navbar as a separate component

---
+++ template/src/js/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class LandingPage extends React.Component {
+  constructor(props) {
+  	super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>This is Landing Page</h2>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
