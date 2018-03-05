21ae097e602bcb606892d939f1f83275154d8d9a
John Olubori David <daveholuborhee@gmail.com>
2018-03-05 13:04:29 +0100

    Add Filter Component

--- template/src/components/SearchPage.jsx
+++ template/src/components/SearchPage.jsx
@@ -2,6 +2,85 @@ import React from 'react';
 import Paper from 'material-ui/Paper';
 import SearchBar from './SearchBar.jsx';
 
+import RaisedButton from 'material-ui/RaisedButton';
+import ActionAndroid from 'material-ui/svg-icons/action/android';
+
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+
+
+const style = {
+  margin: '12px',
+};
+
+
+class Filter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { openPop: false };
+
+    this.handleMoreClick = this.handleMoreClick.bind(this);
+  }
+
+
+  handleMoreClick(event) {
+  	event.preventDefault();
+
+  	this.state.openPop ? this.handleMoreClose : this.setState({ openPop: true, anchorEl: event.currentTarget });
+  }
+
+  handleMoreClose = () => {
+    this.setState({
+      openPop: false,
+    });
+  };
+
+  render() {
+  	return (
+    <Paper className="flex justify-space-evenly">
+      <span className="flex w-50 justify-space-evenly align-items-center">
+        <p className="small" >Filter</p>
+        <span className="flex justify-space-around">
+          {
+  	          	[1, 2, 3, 4].map(i => (
+    <RaisedButton
+      backgroundColor="#f2f2f2"
+      icon={<ActionAndroid />}
+      style={style}
+      key={i}
+    />
+  	          	))
+  	          }
+        </span>
+        <span style={{ alignSelf: 'center' }}>
+          <RaisedButton
+            onClick={this.handleMoreClick}
+            label="More"
+          />
+          <Popover
+            open={this.state.openPop}
+            anchorEl={this.state.anchorEl}
+            anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+            targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+            onRequestClose={this.handleMoreClose}
+          >
+            <Menu>
+              <MenuItem primaryText="Refresh" />
+              <MenuItem primaryText="Help &amp; feedback" />
+              <MenuItem primaryText="Settings" />
+              <MenuItem primaryText="Sign out" />
+            </Menu>
+          </Popover>
+        </span>
+      </span>
+
+      <span />
+    </Paper>
+  	);
+  }
+}
+
 
 class SearchPage extends React.Component {
   constructor(props) {
@@ -16,7 +95,7 @@ class SearchPage extends React.Component {
           <span><SearchBar /></span>
           <nav />
         </div>
-        <Paper>filter</Paper>
+        <Filter />
         <main className="main">
     			Content
         </main>
