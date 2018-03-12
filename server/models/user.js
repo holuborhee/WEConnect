1ee718db50f7597b9309e88538fe94dad670ac11
John Olubori David <daveholuborhee@gmail.com>
2018-03-12 13:46:41 +0100

    Resolve setup of packages

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+import db from '../db';
+
+const { users } = db;
+class User {
+  static add(props) {
+    const user = props;
+    user.id = users.length + 1;
+    users.push(user);
+    return user;
+  }
+
+  static find(id) {
+    const i = parseInt(id, 10);
+    const user = users.find(u => u.id === i);
+    return user;
+  }
+
+  static all() {
+  	return users;
+  }
+}
+
+export default User;
