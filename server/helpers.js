6d0af76394d4e76959cfe5143bddca1af787110e
John Olubori David <daveholuborhee@gmail.com>
2018-03-13 09:06:57 +0100

    Add function propsNotIn to Helper class

--- server/helpers.js
+++ server/helpers.js
@@ -2,3 +2,10 @@
 
 /* . This file contains functions of general use to the whole of the app, */
 
+class Helper {
+	 static propsNotIn(obj, props) {
+	 	return props.filter(p => !Object.prototype.hasOwnProperty.call(obj, p));
+	 }
+}
+
+export default Helper;
