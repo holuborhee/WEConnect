9a5f438431389b78f1f5f45539952bd676f5a3d9
John Olubori David <daveholuborhee@gmail.com>
2018-03-14 11:51:34 +0100

    Remove Test for index url

--- server/tests/index.js
+++ server/tests/index.js
@@ -7,17 +7,6 @@ chai.use(chaiHttp);
 
 
 describe('Routes', () => {
-  describe('/ - index URL', () => {
-    it('should return a 200 code', (done) => {
-      chai.request(app)
-		  .get('/')
-		  .end((err, res) => {
-		  	expect(res).to.have.status(200);
-		  	done();
-		  });
-    });
-  });
-
   describe('/user - user path without parameter', () => {
     it('/GET should be successfull', (done) => {
       chai.request(app)
