ab13ba1cf421bf6ac6f4fa429d65c81c37c5ceb3
John Olubori David <daveholuborhee@gmail.com>
2018-03-15 09:06:51 +0100

    Add return to function show()

--- server/controllers/businessController.js
+++ server/controllers/businessController.js
@@ -19,17 +19,9 @@ class BusinessController {
   static index(req, res) {
     const { q, location, category } = req.query;
     let businesses = Business.all();
-
-    if (q) {
-      businesses = Business.nameHas(q);
-    }
+    if (q) { businesses = Business.nameHas(q); }
     if (location) { businesses = Business.at(location, businesses); }
     if (category) { businesses = Business.under(category, businesses); }
-
-    /* return res.status(200).send({
-      message,
-      error: false,
-    }); */
     status = 200;
     response.data = { businesses };
     return res.status(status).send(response);
@@ -96,7 +88,7 @@ class BusinessController {
     response.data = business ? { business } : { id: `No resource could be found for ${id} on the server` };
     response.status = business ? 'success' : 'fail';
     status = business ? 200 : 404;
-    res.status(status).send(response);
+    return res.status(status).send(response);
   }
 
 
