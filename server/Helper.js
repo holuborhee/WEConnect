7083a6c1da766e4d523e602cdeabdc8b9e802880
John Olubori David <daveholuborhee@gmail.com>
2018-03-25 20:06:10 +0100

    Add Helper.js

---
+++ server/Helper.js
@@ -0,0 +1,28 @@
+
+
+/* . This file contains functions of general use to the whole of the app, */
+
+class Helper {
+  static propsNotIn(obj, props) {
+    return props.filter(p => !Object.prototype.hasOwnProperty.call(obj, p) || obj[p] === '');
+  }
+
+
+  static validateRequiredInRequest(requestBody, required) {
+    const notFound = Helper.propsNotIn(requestBody, required);
+    const response = {};
+    if (notFound.length > 0) {
+      response.status = 'fail';
+      response.data = {};
+      /* eslint array-callback-return: "off" */
+      notFound.map((el) => {
+        response.data[el] = `${el} is required`;
+      });
+      return response;
+    }
+
+    return true;
+  }
+}
+
+export default Helper;
