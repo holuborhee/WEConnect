f7d4e1348854b3fe7c1dfe469a62a5d950d153d9
John Olubori David <daveholuborhee@gmail.com>
2018-03-26 10:05:04 +0100

    Add models

--- server/models/User.js
+++ server/models/User.js
@@ -1,57 +1,38 @@
-import db from '../db';
-import Helper from '../Helper';
 
-const { users } = db;
-class User {
-  static add(props) {
-    const user = props;
-    const required = ['name', 'phone', 'email', 'password'];
-    if (Helper.propsNotIn(user, required).length > 0) { return null; }
-    user.id = users.length + 1;
-    users.push(user);
-    return user;
-  }
-
-  static find(id) {
-    const i = parseInt(id, 10);
-    const user = users.find(u => u.id === i);
-    return user;
-  }
-
-  static getByEmailAndPassword(email, password) {
-    const user = User.getByEmail(email);
-    if (user) {
-      if (user.password === password) {
-        return user;
-      } return { password: `${password} isn't matching for the user` };
-    }
-
-    return { email: `${email} doesn't match any email on server` };
-  }
-
-
-  static getByPhoneAndPassword(phone, password) {
-    const user = User.getByPhone(phone);
-    if (user) {
-      if (user.password === password) {
-        return user;
-      } return { password: `${password} isn't matching for the user` };
-    }
-
-    return { phone: `${phone} doesn't match any phone on server` };
-  }
-
-  static getByEmail(email) {
-    return users.find(u => u.email === email);
-  }
-
-  static getByPhone(phone) {
-    return users.find(u => u.phone === phone);
-  }
-
-  static all() {
-  	return users;
-  }
-}
-
-export default User;
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'Phone number already in use!',
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+      unique: {
+        args: true,
+        msg: 'Email address already in use!',
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  }, {});
+  User.associate = (models) => {
+    User.hasOne(models.Business, {
+      foreignKey: 'userId',
+    });
+  };
+  return User;
+};
