4360ab35f56deb77d05ace89d2ea5cfd56b39d11
John Olubori David <daveholuborhee@gmail.com>
2018-03-26 09:21:59 +0100

    Add migrations

---
+++ server/migrations/20180318072311-create-business.js
@@ -0,0 +1,55 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Businesses', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    latitude: {
+      type: Sequelize.DOUBLE,
+      allowNull: false,
+    },
+    longitude: {
+      type: Sequelize.DOUBLE,
+      allowNull: false,
+    },
+    address: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    },
+    categoryId: {
+      type: Sequelize.INTEGER,
+      onDelete: 'CASCADE',
+      allowNull: false,
+      references: {
+        model: 'Categories',
+        key: 'id',
+      },
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Businesses'),
+};
