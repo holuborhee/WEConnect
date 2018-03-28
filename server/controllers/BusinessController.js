c961787106eef09ad041139cc0e7ec066b024411
John Olubori David <daveholuborhee@gmail.com>
2018-03-28 17:50:44 +0100

    Implement create a new Review

--- server/controllers/BusinessController.js
+++ server/controllers/BusinessController.js
@@ -173,37 +173,26 @@ class BusinessController {
       .then((business) => {
         if (business) {
           Review.findAll({ where: { businessId: business.id } })
-            .then(reviews => res.status(200).send({ status: 'success', data: { reviews } }));
+            .then(reviews => res.status(200).send({ status: 'success', data: { reviews } }))
+            .catch(err => res.status(500).send({ status: 'error', message: 'There was an internal server error' }));
         } else return res.status(404).send({ status: 'fail', data: { id: `No resource could be found for ${id} on the server` } });
-      });
+      }).catch(err => res.status(500).send({ status: 'error', message: 'There was an internal server error' }));
   }
 
   static newReview(req, res) {
     const { id } = req.params;
 
-    const business = Business.find(id);
-    if (business) {
-      /* Implementation here will be refractored to a ReviewController */
-      const required = ['reviewer', 'comment', 'rating'];
-      const resp = Helper.validateRequiredInRequest(req.body, required);
-      if (resp !== true) {
-        response.data = resp.data;
-        response.status = resp.status;
-        status = 422;
-      } else {
-        const { reviewer, comment, rating } = req.body;
-        const review = business.review.add({ reviewer, comment, rating });
-        response.data = { review };
-        response.status = 'success';
-        status = 201;
-      }
-    } else {
-      response.data = { id: `No resource could be found for ${id} on the server` };
-      response.status = 'fail';
-      status = 404;
-    }
-
-    return res.status(status).send(response);
+    Business.findById(id)
+      .then((business) => {
+        if (business) {
+          const { reviewer, comment, rating } = req.body;
+          Review.create({
+            reviewer, comment, rating, businessId: business.id,
+          })
+            .then(review => res.status(201).send({ status: 'success', data: { review } }))
+            .catch(err => res.status(500).send({ status: 'error', message: 'There was an internal server error' }));
+        } else return res.status(404).send({ status: 'fail', data: { id: `No resource could be found for ${id} on the server` } });
+      }).catch(err => res.status(500).send({ status: 'error', message: 'There was an internal server error' }));
   }
 }
 
